081e3b99aabad4f0dec74f305302b4f5edb567da
Benicio <gustavo.benicio@kiwify.com.br>
2026-01-15 18:28:41 -0300

    add send message 2

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import { fastify } from "fastify";
 import { createTask } from "./routes/createTask";
 import { homeRoutes } from "./routes/home";
 import { debugTables } from "./routes/debugTables";
+import { sendMessage } from "./routes/sendMessage";
 
 const app = fastify()
 
 app.register(createTask)
 app.register(homeRoutes)
 app.register(debugTables)
+app.register(sendMessage)
+
 
 const port = Number(process.env.PORT ?? 3030);
 
