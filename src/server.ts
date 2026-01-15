0a0d70577609bc5dc049cb4d4b3e6bb6162f1bb5
Benicio <gustavo.benicio@kiwify.com.br>
2026-01-15 10:36:11 -0300

    port fix

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,9 @@ const app = fastify()
 app.register(createTask)
 app.register(homeRoutes)
 
-app.get("/health", ()=>{
-    return "Olas"
-})
 
-app.listen({ port: 3333, host: "0.0.0.0" }).then(()=>{
-    console.log("HTTP Server is Running")
-})
+const port = Number(process.env.PORT ?? 3030);
+
+app.listen({ port, host: "0.0.0.0" }).then(() => {
+  console.log(`HTTP server running on ${port}`);
+});
