2829bc14313947c7116e8a3840a53d3e02d3837d
Benicio <gustavo.benicio@kiwify.com.br>
2026-01-15 10:31:47 -0300

    migrate db

--- prisma/migrations/20260115133120_init/migration.sql
+++ prisma/migrations/20260115133120_init/migration.sql
@@ -1,23 +1,26 @@
 -- CreateTable
 CREATE TABLE "tasks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT,
     "delegation" TEXT NOT NULL,
     "responsible" TEXT NOT NULL,
-    "term" DATETIME,
-    "recurrence" TEXT NOT NULL,
+    "term" TIMESTAMP(3),
+    "recurrence" TEXT,
     "urgency" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "task-carbon-copies" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "taskId" TEXT NOT NULL,
     "slackUserId" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT "task-carbon-copies_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "task-carbon-copies_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -25,3 +28,6 @@ CREATE INDEX "task-carbon-copies_slackUserId_idx" ON "task-carbon-copies"("slack
 
 -- CreateIndex
 CREATE UNIQUE INDEX "task-carbon-copies_taskId_slackUserId_key" ON "task-carbon-copies"("taskId", "slackUserId");
+
+-- AddForeignKey
+ALTER TABLE "task-carbon-copies" ADD CONSTRAINT "task-carbon-copies_taskId_fkey" FOREIGN KEY ("taskId") REFERENCES "tasks"("id") ON DELETE CASCADE ON UPDATE CASCADE;
